090a0b74e8192da9498131df6c01d2fe03ec1e0b
Nik <61439310+NikV2@users.noreply.github.com>
2022-05-03 15:32:56 +0300

    Fixed AimAssist MouseY, And added more options. Enjoy!

--- src/main/java/dev/africa/pandaware/impl/module/combat/AimAssistModule.java
+++ src/main/java/dev/africa/pandaware/impl/module/combat/AimAssistModule.java
@@ -7,7 +7,10 @@ import dev.africa.pandaware.api.module.Module;
 import dev.africa.pandaware.api.module.interfaces.Category;
 import dev.africa.pandaware.api.module.interfaces.ModuleInfo;
 import dev.africa.pandaware.impl.event.player.MotionEvent;
+import dev.africa.pandaware.impl.setting.BooleanSetting;
+import dev.africa.pandaware.impl.setting.EnumSetting;
 import dev.africa.pandaware.impl.setting.NumberSetting;
+import dev.africa.pandaware.utils.math.random.RandomUtils;
 import dev.africa.pandaware.utils.math.vector.Vec2f;
 import dev.africa.pandaware.utils.player.RotationUtils;
 import lombok.Getter;
@@ -23,13 +26,19 @@ public class AimAssistModule extends Module {
 
     private final NumberSetting distance = new NumberSetting("Distance", 10D, 1D, 4D, 0.5D);
     private final NumberSetting smoothing = new NumberSetting("Smoothing", 2F, 0.1F, 2F, 0.1F);
+    private final EnumSetting<RotationUtils.RotationAt> lookAt
+            = new EnumSetting<>("Look At", RotationUtils.RotationAt.HEAD);
+
+    private final BooleanSetting randomizeAimPoint = new BooleanSetting("Randomize Aim Point", false);
 
     public static int mouseX, mouseY;
 
     public AimAssistModule() {
         this.registerSettings(
                 this.distance,
-                this.smoothing
+                this.smoothing,
+                this.lookAt,
+                this.randomizeAimPoint
         );
     }
 
@@ -66,8 +75,16 @@ public class AimAssistModule extends Module {
         Vec2f targetRotations = RotationUtils.getRotations(target);
         Vec2f currentRotations = new Vec2f(mc.thePlayer.rotationYaw, mc.thePlayer.rotationPitch);
 
+        RotationUtils.RotationAt rotationAt = (this.randomizeAimPoint.getValue() ?
+                RotationUtils.RotationAt.values()
+                        [RandomUtils.nextInt(0, RotationUtils.RotationAt.values().length - 1)] :
+                this.lookAt.getValue());
+
         float offsetX = MathHelper.wrapAngleTo180_float(targetRotations.getX() - currentRotations.getX());
-        float offsetY = MathHelper.clamp_float(targetRotations.getY() - currentRotations.getY(), -90F, 90F);
+        float offsetY = MathHelper.clamp_float(
+                currentRotations.getY() - RotationUtils.getMiddlePointRotations(target, rotationAt).getY(),
+                -90F,
+                90F);
 
         float fpt = Minecraft.getDebugFPS() / 20F;
 
@@ -80,6 +97,6 @@ public class AimAssistModule extends Module {
         float f1 = f * f * f * 8.0F;
 
         mouseX = Math.round(smoothX / f1);
-        //TODO: Fix Y
+        mouseY = Math.round(smoothY / f1);
     };
 }
